d763c4f220c41fe4f0eb1bf4c185f371fb6f29fa
eddofr <103290823+eddofr@users.noreply.github.com>
2023-12-20 20:51:37 -0500

    commit con la prueba para consumir api exitosa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'spotifyapp';
+  title = 'edson';
+
+  constructor(){
+    console.log("Edson");
+  }
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  
+  paises : any[] = [];
+
+  constructor( private http : HttpClient){
+    this.http.get('https://restcountries.com/v3.1/lang/spanish')
+             .subscribe((data : any) => {
+              this.paises = data;
+             });
+  }
+
+  ngOnInit(): void {
+
+    
+  }
 
 }
